4512247e87ee6c163433ba04ccb903dded41d42f
Lea <Lea@147.172.177.38>
2018-11-26 14:19:31 +0100

    Different setup for Windows

--- python/joern-tools/setup.py
+++ python/joern-tools/setup.py
@@ -1,7 +1,12 @@
-import os, sys
+import os, sys, fnmatch, platform 
 
-from setuptools import setup, find_packages
-import os, fnmatch
+from platform import linux_distribution
+
+
+#Only if Linux
+if (platform.system() == linux_distribution):
+    from setuptools import setup, find_packages
+
 
 if (sys.version_info.major, sys.version_info.minor) < (3, 4):
     sys.exit("Python < 3.4 not supported.")
@@ -16,18 +21,20 @@ def getScripts():
             x.extend(os.path.join(dirpath,f) for f in filenames)
     return x
 
-setup(
-    name = "joern",
-    version = "0.3",
-    author = "Fabian Yamaguchi",
-    author_email = "f.yamaguchi@tu-braunschweig.de",
-    description = "Tools for code analysis based on joern.",
-    license = "GPLv3",
-    url = "http://github.com/octopus-platform/joern-tools/",
-    long_description = read('README.md'),
-    packages = find_packages(),
-    data_files=[ ('EGG-INFO/scripts/data', ['scripts/plotting/data/plotconfig.cfg'])],
-    scripts = getScripts(),
-    install_requires = ['pygraphviz', 'chardet'],
-    zip_safe = False
-)
+#Only if Linux
+if (platform.system() == linux_distribution):
+    setup(
+        name = "joern",
+        version = "0.3",
+        author = "Fabian Yamaguchi",
+        author_email = "f.yamaguchi@tu-braunschweig.de",
+        description = "Tools for code analysis based on joern.",
+        license = "GPLv3",
+        url = "http://github.com/octopus-platform/joern-tools/",
+        long_description = read('README.md'),
+        packages = find_packages(),
+        data_files=[ ('EGG-INFO/scripts/data', ['scripts/plotting/data/plotconfig.cfg'])],
+        scripts = getScripts(),
+        install_requires = ['pygraphviz', 'chardet'],
+        zip_safe = False
+    )
