8a835247ba4be2a3942be66b43c75a65c729dac7
Lea <Lea@147.172.177.46>
2018-12-17 16:42:00 +0100

    Added a test class

---
+++ testProjects/JoernTests.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : JoernTests.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int global;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
+
+void testWhileAndInc() {
+
+	int i = 0;
+	while (i > 5){
+		i++;
+	}
+
+	global = 5;
+}
