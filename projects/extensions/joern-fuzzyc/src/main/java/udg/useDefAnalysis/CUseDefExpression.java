68cf8ad5f630dbee5781bdab1120df59b937a7ae
Lea <Lea@147.172.177.46>
2018-12-17 15:35:55 +0100

    Another attempt to remove pointer logic from symbols

--- projects/extensions/joern-fuzzyc/src/main/java/udg/useDefAnalysis/CUseDefExpression.java
+++ projects/extensions/joern-fuzzyc/src/main/java/udg/useDefAnalysis/CUseDefExpression.java
@@ -2,11 +2,11 @@ package udg.useDefAnalysis;
 
 public class CUseDefExpression {
     /**
-     * Eliminate redundant "& * " and "* & " patterns that can occur in
-     * dynamically generated UseDef references.
+     * Eliminate all & and *, because we do not need this information for 
+     * Semantic Unit identification (and it makes things more complicated).
      */
     public static String simplify(String expr)
     {
-        return expr.replace("& * ", "").replace("* & ", "");
+        return expr.replace("&", "").replace("*", "");
     }
 }
