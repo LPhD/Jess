98de365750b798602eae28bbd221195ebe4111e0
Lea <Lea@147.172.177.38>
2019-02-08 12:16:27 +0100

    Adapted preprocessor tests for the module parser

--- projects/extensions/joern-fuzzyc/src/test/java/tests/languages/c/antlrParsers/moduleParser/PreprocessorTests.java
+++ projects/extensions/joern-fuzzyc/src/test/java/tests/languages/c/antlrParsers/moduleParser/PreprocessorTests.java
@@ -14,17 +14,17 @@ public class PreprocessorTests extends FunctionDefinitionTests {
 
 		ModuleParser parser = createParser(input);
 		String output = parser.function_def().toStringTree(parser);
-		String outputExpected = "\"(function_def \")";
+		String outputExpected = "(function_def (return_type (type_name (base_type int))) (function_name (identifier foo)) (function_param_list ( )) (compound_statement { #if bar int i ; #endif }))";
 		assertEquals(outputExpected, output);
 	}
 
 	@Test
 	public void testNestedPreprocessorIfs() {
-		String input = "int foo(){ #if bar\n #if bar2\n { #endif\n #endif\n}";
+		String input = "int foo(){ #if bar\n #if bar2\n #endif\n #endif\n}";
 
 		ModuleParser parser = createParser(input);
 		String output = parser.function_def().toStringTree(parser);
-		String outputExpected = "\"(function_def \"";
+		String outputExpected = "(function_def (return_type (type_name (base_type int))) (function_name (identifier foo)) (function_param_list ( )) (compound_statement { #if bar #if bar2 #endif #endif }))";
 		assertEquals(outputExpected, output);
 	}
 
@@ -33,16 +33,17 @@ public class PreprocessorTests extends FunctionDefinitionTests {
 		String input = "#ifdef foo\n int foo(){ #if x\n foo();\n #else\n #endif\n } abc\n #endif\n";
 		ModuleParser parser = createParser(input);
 		String output = parser.code().toStringTree(parser);
-		String outputExpected = "(water abc)";
+		String outputExpected = "(code (pre_statement (pre_blockstarter (pre_if_statement #ifdef (pre_if_condition (condition (expr (assign_expr (conditional_expression (or_expression (and_expression (inclusive_or_expression (exclusive_or_expression (bit_and_expression (equality_expression (relational_expression (shift_expression (additive_expression (multiplicative_expression (cast_expression (unary_expression (postfix_expression (primary_expression (identifier foo))))))))))))))))))))))) "
+				+ "(function_def (return_type (type_name (base_type int))) (function_name (identifier foo)) (function_param_list ( )) (compound_statement { #if x foo ( ) ; #else #endif })) (water abc) (pre_statement (pre_blockstarter (pre_endif_statement #endif))))";
 		assertEquals(outputExpected, output);
 	}
 
 	@Test
 	public void testPreprocIfNesting() {
-		String input = "foo(){ #ifdef x\n #ifdef y\n #else\n #endif\n #endif\n abc(); } foo();";
+		String input = "int foo(){ #ifdef x\n #ifdef y\n #else\n #endif\n #endif\n abc(); } foo();";
 		ModuleParser parser = createParser(input);
 		String output = parser.code().toStringTree(parser);
-		String outputExpected = "(compound_statement { #ifdef x\\n #ifdef y\\n #else\\n #endif\\n #endif\\n abc ( ) ; }))";
+		String outputExpected = "(code (function_def (return_type (type_name (base_type int))) (function_name (identifier foo)) (function_param_list ( )) (compound_statement { #ifdef x #ifdef y #else #endif #endif abc ( ) ; })) (water foo) (water () (water )) (water ;))";
 		assertEquals(outputExpected, output);
 	}
 
@@ -51,7 +52,8 @@ public class PreprocessorTests extends FunctionDefinitionTests {
 		String input = "#ifdef x\n int i;  #else\n int x; #endif\n ";
 		ModuleParser parser = createParser(input);
 		String output = parser.code().toStringTree(parser);
-		String outputExpected = "";
+		String outputExpected = "(code (pre_statement (pre_blockstarter (pre_if_statement #ifdef (pre_if_condition (condition (expr (assign_expr (conditional_expression (or_expression (and_expression (inclusive_or_expression (exclusive_or_expression (bit_and_expression (equality_expression (relational_expression (shift_expression (additive_expression (multiplicative_expression (cast_expression (unary_expression (postfix_expression (primary_expression (identifier x))))))))))))))))))))))) "
+				+ "(simple_decl (var_decl (type_name (base_type int)) (init_declarator_list (init_declarator (declarator (identifier i))) ;))) (pre_statement (pre_blockstarter (pre_else_statement #else))) (simple_decl (var_decl (type_name (base_type int)) (init_declarator_list (init_declarator (declarator (identifier x))) ;))) (pre_statement (pre_blockstarter (pre_endif_statement #endif))))";
 		assertEquals(outputExpected, output);
 	}
 
@@ -60,16 +62,16 @@ public class PreprocessorTests extends FunctionDefinitionTests {
 		String input = "foo(){ #ifdef x\n #else\n #ifdef y\n #endif\n #endif\n abc(); } foo();";
 		ModuleParser parser = createParser(input);
 		String output = parser.code().toStringTree(parser);
-		String outputExpected = "(compound_statement { #ifdef x\\n #else\\n #ifdef y\\n #endif\\n #endif\\n abc ( ) ; }))";
+		String outputExpected = "(code (function_def (function_name (identifier foo)) (function_param_list ( )) (compound_statement { #ifdef x #else #ifdef y #endif #endif abc ( ) ; })) (water foo) (water () (water )) (water ;))";
 		assertEquals(outputExpected, output);
 	}
 
 	@Test
-	public void testStartingPreProcElse() {
-		String input = "#ifdef foo\n int foo(){ #else\n {\n#endif\n } abc\n #endif\n";
+	public void testScatteredPreProc() {
+		String input = "#ifdef foo\n int foo(){ #else\n {\n #endif\n } abc\n #endif\n";
 		ModuleParser parser = createParser(input);
 		String output = parser.code().toStringTree(parser);
-		String outputExpected = "(water abc)";
+		String outputExpected = "(code (pre_statement (pre_blockstarter (pre_if_statement #ifdef (pre_if_condition (condition (expr (assign_expr (conditional_expression (or_expression (and_expression (inclusive_or_expression (exclusive_or_expression (bit_and_expression (equality_expression (relational_expression (shift_expression (additive_expression (multiplicative_expression (cast_expression (unary_expression (postfix_expression (primary_expression (identifier foo))))))))))))))))))))))) (function_def (return_type (type_name (base_type int))) (function_name (identifier foo)) (function_param_list ( )) (compound_statement { #else { #endif })) (water abc) (pre_statement (pre_blockstarter (pre_endif_statement #endif))))";
 		assertEquals(outputExpected, output);
 	}
 
