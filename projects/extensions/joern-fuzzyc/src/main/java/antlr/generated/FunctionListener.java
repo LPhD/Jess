7c394775741f534bbeb068715b0390d9b8207099
gerling <gerling@Gerling-7490.sse.local>
2019-02-14 12:19:50 +0100

    Refactored antlr files

--- projects/extensions/joern-fuzzyc/src/main/java/antlr/generated/FunctionListener.java
+++ projects/extensions/joern-fuzzyc/src/main/java/antlr/generated/FunctionListener.java
@@ -1,4 +1,4 @@
-// Generated from ..\java\antlr\Function.g4 by ANTLR 4.5.3
+// Generated from ..\java\antlr\grammar\Function.g4 by ANTLR 4.5.3
 
 	package antlr.generated;
 
--- projects/extensions/joern-fuzzyc/src/main/java/antlr/generated/ModuleListener.java
+++ projects/extensions/joern-fuzzyc/src/main/java/antlr/generated/ModuleListener.java
@@ -1,4 +1,4 @@
-// Generated from ..\java\antlr\Module.g4 by ANTLR 4.5.3
+// Generated from ..\java\antlr\grammar\Module.g4 by ANTLR 4.5.3
 
 	package antlr.generated;
 
