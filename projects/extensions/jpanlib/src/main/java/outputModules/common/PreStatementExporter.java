3d45d631f943deacab1ad986f23ede85ed0b6783
Lea <Lea@147.172.177.38>
2019-02-20 12:30:24 +0100

    Revert "Debugging outputs"
    This reverts commit 74b806c2d02623e81ae05b7c6f0f283ad7fd4365.

--- projects/extensions/jpanlib/src/main/java/outputModules/common/PreStatementExporter.java
+++ projects/extensions/jpanlib/src/main/java/outputModules/common/PreStatementExporter.java
@@ -21,12 +21,13 @@ public abstract class PreStatementExporter extends ASTNodeExporter{
 		try	{			
 			preDBNode.initialize(preASTNode);
 			addMainNode(preDBNode);
-			//Connect with parent
-			linkPreStatementToFileNode(preDBNode, curFile);	
 			
 			//new
 			astImporter.addASTChildren(preASTNode);
-						
+			
+			//Connect with parent
+			linkPreStatementToFileNode(preDBNode, curFile);	
+			
 //			//Add condition for pre-if and -elif statements
 //			String type = preASTNode.getTypeAsString();
 //			if (type.equals("PreIfStatement") || type.equals("PreElIfStatement"))
--- projects/extensions/jpanlib/src/main/java/outputModules/csv/exporters/CSVASTExporter.java
+++ projects/extensions/jpanlib/src/main/java/outputModules/csv/exporters/CSVASTExporter.java
@@ -9,31 +9,27 @@ import databaseNodes.NodeKeys;
 import outputModules.common.ASTExporter;
 import outputModules.common.Writer;
 
-public class CSVASTExporter extends ASTExporter {
+public class CSVASTExporter extends ASTExporter
+{
 
 	@Override
-	protected void addASTLink(ASTNode parent, ASTNode child) {
-		System.out.println("Parent node: " + parent);
-		System.out.println("child node: " + child);
-		System.out.println("Parent node id: " + Writer.getIdForObject(parent));
-		System.out.println("child node id: " + Writer.getIdForObject(child));
-		System.out.println("Parent node code: " + parent.getEscapedCodeStr());
-		System.out.println("child node code: " + child.getEscapedCodeStr());
-		
+	protected void addASTLink(ASTNode parent, ASTNode child)
+	{
 		long srcId = Writer.getIdForObject(parent);
 		long dstId = Writer.getIdForObject(child);
 		Writer.addEdge(srcId, dstId, null, EdgeTypes.IS_AST_PARENT);
 	}
 
 	@Override
-	protected void addASTNode(ASTNode node) {
+	protected void addASTNode(ASTNode node)
+	{
 		ASTDatabaseNode astDatabaseNode = new ASTDatabaseNode();
 		astDatabaseNode.initialize(node);
 		astDatabaseNode.setCurrentFunction(currentFunction);
 		astDatabaseNode.setInsideFunctionBlock(this.isInsideFunctionBlock());
 		Map<String, Object> properties = astDatabaseNode.createProperties();
-		// TODO: currentFunction can be empty if we are inside a pre statement
-		if (currentFunction != null)
+		//TODO: currentFunction can be empty if we are inside a pre statement
+		if(currentFunction != null)
 			properties.put(NodeKeys.FUNCTION_ID, Writer.getIdForObject(currentFunction).toString());
 		Writer.addNode(node, properties);
 	}
