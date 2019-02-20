86e34fe5ce428100e1b628d2be4563289589db06
Lea <Lea@147.172.177.38>
2019-02-20 12:30:22 +0100

    Revert "Removed unused methods"
    This reverts commit b79567719c286e92fd32af24bb64ad4c5973d49a.

--- projects/extensions/jpanlib/src/main/java/outputModules/common/PreStatementExporter.java
+++ projects/extensions/jpanlib/src/main/java/outputModules/common/PreStatementExporter.java
@@ -1,7 +1,10 @@
 package outputModules.common;
 
 import ast.ASTNode;
+import ast.preprocessor.PreStatementBase;
+import databaseNodes.DatabaseNode;
 import databaseNodes.FileDatabaseNode;
+import databaseNodes.PreConditionDatabaseNode;
 import databaseNodes.PreStatementDatabaseNode;
 
 public abstract class PreStatementExporter extends ASTNodeExporter{
@@ -9,8 +12,11 @@ public abstract class PreStatementExporter extends ASTNodeExporter{
 	protected ASTExporter astImporter;
 	
 	@Override
-
-	public void addToDatabaseSafe(ASTNode preASTNode)	{
+	public void addToDatabaseSafe(ASTNode node)	{
+		addPreStatementToDatabaseSafe((PreStatementBase) node);
+	}
+	
+	public void addPreStatementToDatabaseSafe(PreStatementBase preASTNode)	{
 		PreStatementDatabaseNode preDBNode = new PreStatementDatabaseNode();
 		try	{			
 			preDBNode.initialize(preASTNode);
@@ -21,6 +27,11 @@ public abstract class PreStatementExporter extends ASTNodeExporter{
 			//new
 			astImporter.addASTToDatabase(preASTNode);
 						
+//			//Add condition for pre-if and -elif statements
+//			String type = preASTNode.getTypeAsString();
+//			if (type.equals("PreIfStatement") || type.equals("PreElIfStatement"))
+//				addCondition(preDBNode, preASTNode);
+
 			
 		} catch (RuntimeException ex)	{
 			ex.printStackTrace();
@@ -28,7 +39,47 @@ public abstract class PreStatementExporter extends ASTNodeExporter{
 			return;
 		}
 	}
-		
-	protected abstract void linkPreStatementToFileNode(PreStatementDatabaseNode classDefNode, FileDatabaseNode fileNode);	
+	
+	
+	public void addCondition(PreStatementDatabaseNode preDBNode, PreStatementBase preASTNode) {
+		try	{			
+			PreConditionDatabaseNode conditionDBNode = new PreConditionDatabaseNode();		
+			//Initialize condition as DB node
+			conditionDBNode.initialize(preASTNode.getChild(0));
+			addASTNode(conditionDBNode);
+			//Link condition DB node with parent pre statement db node
+			addASTLink(preDBNode, conditionDBNode);		
+		} catch (RuntimeException ex)	{
+			ex.printStackTrace();
+			System.err.println("Error adding pre-statement condition to database: "+ preDBNode.toString());
+			return;
+		}	
+	}
+	
+
+
+	protected abstract void addASTLink(PreStatementDatabaseNode parent, PreConditionDatabaseNode child);
+	protected abstract void addASTNode(DatabaseNode dbNode);
+	protected abstract void linkPreStatementToFileNode(PreStatementDatabaseNode classDefNode, FileDatabaseNode fileNode);
+	
+//	/**
+//	 * Add current ASTNode to database and look for its children
+//	 * @param currentNode The current node.
+//	 * @param parent The parent node of the current node.
+//	 */
+//	public void addASTToDatabase(ASTNode parent, ASTNode currentNode) {
+//		ASTDatabaseNode astDatabaseNode = new ASTDatabaseNode();
+//		try	{
+//			astDatabaseNode.initialize(currentNode);
+//			addASTNode(astDatabaseNode);
+//			addASTLink(parent, astDatabaseNode);
+//			addASTChildren(astDatabaseNode);
+//		
+//		} catch (RuntimeException ex)	{
+//			ex.printStackTrace();
+//			System.err.println("Error adding pre-statement children to database: "+ astDatabaseNode.toString());
+//			return;
+//		}		
+//	}
 
 }
