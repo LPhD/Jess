dbf85ec69d503efd0244a294b4613f40387e999b
Lea <Lea@147.172.177.42>
2019-03-01 11:55:43 +0100

    Removed changes

--- projects/extensions/jpanlib/src/main/java/outputModules/csv/ParserCSVOutput.java
+++ projects/extensions/jpanlib/src/main/java/outputModules/csv/ParserCSVOutput.java
@@ -1,25 +1,28 @@
 package outputModules.csv;
 
 import java.io.File;
-import java.nio.file.Files;
 
 import outputModules.CSVASTWalker;
 import outputModules.common.Writer;
 import outputModules.csv.exporters.CSVFunctionExporter;
 import outputModules.parser.Parser;
 
-public abstract class ParserCSVOutput extends Parser {
+
+public abstract class ParserCSVOutput extends Parser
+{
 
 	private CSVFunctionExporter functionExporter;
 	boolean multiFileOutput = true;
 
-	public void setMultiFileOutput(boolean multiFileOutput) {
+	public void setMultiFileOutput(boolean multiFileOutput)
+	{
 		this.multiFileOutput = multiFileOutput;
 	}
 
 	@Override
-	public void initialize() {
-		if (multiFileOutput)
+	public void initialize()
+	{
+		if(multiFileOutput)
 			Writer.setWriterImpl(new MultiPairCSVWriterImpl());
 		else
 			Writer.setWriterImpl(new SinglePairCSVWriterImpl());
@@ -28,62 +31,56 @@ public abstract class ParserCSVOutput extends Parser {
 	}
 
 	@Override
-	protected void initializeDatabase() {
+	protected void initializeDatabase()
+	{
 		createOutputDirectory();
 	}
 
-	private void createOutputDirectory() {
+	private void createOutputDirectory()
+	{
 		File outputDirectory = new File(outputDir);
-		if (outputDirectory.exists()) {
-			try {
-				System.out.println("Output directory already exists, trying to overwrite...");
-				deleteDir(outputDirectory);
-			} catch (Exception e) {
-				throw new RuntimeException("Output directory already exists and cant be removed");
-			}
-		} else {
-			try {
-				outputDirectory.mkdirs();
-			} catch (SecurityException ex) {
-				throw new RuntimeException("Cannot create output directory, permission denied.");
-			}
+		if (outputDirectory.exists())
+			throw new RuntimeException("Output directory already exists");
+
+		try
+		{
+			outputDirectory.mkdirs();
+		} catch (SecurityException ex)
+		{
+			throw new RuntimeException(
+					"Cannot create output directory, permission denied.");
 		}
 	}
 
 	@Override
-	protected void shutdownDatabase() {
+	protected void shutdownDatabase()
+	{
 		Writer.getWriterImpl().shutdown();
 	}
 
 	@Override
-	protected void initializeDirectoryImporter() {
-		if (multiFileOutput)
+	protected void initializeDirectoryImporter()
+	{
+		if(multiFileOutput)
 			dirTreeImporter = new MultiDirCSVDirectoryTreeImporter();
 		else
 			dirTreeImporter = new SingleDirCSVDirectoryTreeImporter();
 	}
 
 	@Override
-	protected void initializeWalker() {
+	protected void initializeWalker()
+	{
 		astWalker = new CSVASTWalker(getFunctionExporter());
 	}
 
-	public CSVFunctionExporter getFunctionExporter() {
+	public CSVFunctionExporter getFunctionExporter()
+	{
 		return functionExporter;
 	}
 
-	public void setFunctionExporter(CSVFunctionExporter functionExporter) {
+	public void setFunctionExporter(CSVFunctionExporter functionExporter)
+	{
 		this.functionExporter = functionExporter;
-	}
-
-	private void deleteDir(File file) {
-		File[] contents = file.listFiles();
-		if (contents != null) {
-			for (File f : contents) {
-				deleteDir(f);
-			}
-		}
-		file.delete();
 	}
 
 }
