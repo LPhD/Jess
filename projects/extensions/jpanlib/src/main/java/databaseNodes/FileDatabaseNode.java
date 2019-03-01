5796bb7b87cb05728d2dc102fd03c066513c37ef
Lea <Lea@147.172.177.42>
2019-03-01 13:58:37 +0100

    Show filename instead of full path

--- projects/extensions/jpanlib/src/main/java/databaseNodes/FileDatabaseNode.java
+++ projects/extensions/jpanlib/src/main/java/databaseNodes/FileDatabaseNode.java
@@ -4,47 +4,47 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FileDatabaseNode extends DatabaseNode
-{
+public class FileDatabaseNode extends DatabaseNode {
 
 	long neo4jId = -1;
 	private String pathString;
 	private String type;
+	private String fileName;
 
-	public void setType(String aType)
-	{
+	public void setType(String aType) {
 		type = aType;
 	}
 
-	public void setId(long anId)
-	{
+	public void setId(long anId) {
 		neo4jId = anId;
 	}
 
-	public long getId()
-	{
+	public long getId() {
 		return neo4jId;
 	}
 
 	@Override
-	public void initialize(Object obj)
-	{
+	public void initialize(Object obj) {
 		Path path = (Path) obj;
 		pathString = path.toString();
+		fileName = path.getFileName().toString();
 	}
 
-	public String getPath()
-	{
+	public String getPath() {
 		return pathString;
 	}
 
+	public String getFileName() {
+		return fileName;
+	}
+	
 	@Override
-	public Map<String, Object> createProperties()
-	{
+	public Map<String, Object> createProperties() {
 		Map<String, Object> properties = new HashMap<String, Object>();
 		properties.put(NodeKeys.NODE_TYPE, type);
-		properties.put(NodeKeys.CODE, pathString);
+		properties.put(NodeKeys.CODE, fileName);
 		return properties;
 	}
+
 
 }
