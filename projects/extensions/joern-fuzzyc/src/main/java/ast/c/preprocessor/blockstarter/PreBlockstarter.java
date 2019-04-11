62cdc1f672cd187a2a7ee03f224f6291ec099d3d
gerling <gerling@Gerling-7490.sse.local>
2019-04-11 13:27:12 +0200

    Separated variable statements from AST children

--- projects/extensions/joern-fuzzyc/src/main/java/ast/c/preprocessor/blockstarter/PreBlockstarter.java
+++ projects/extensions/joern-fuzzyc/src/main/java/ast/c/preprocessor/blockstarter/PreBlockstarter.java
@@ -1,13 +1,21 @@
 package ast.c.preprocessor.blockstarter;
 
+import java.util.LinkedList;
+
+import ast.ASTNode;
 import ast.c.preprocessor.PreStatement;
 import ast.expressions.Expression;
 import ast.logical.statements.Statement;
 import ast.walking.ASTNodeVisitor;
 
-public class PreBlockstarter extends PreStatement{
+public class PreBlockstarter extends PreStatement {
 	protected Expression condition = null;
 	protected Statement statement = null;
+	/**
+	 * Contains all statements that are annotated with the current #if/#elif/#else 
+	 */
+	protected LinkedList<ASTNode> variableStatements;
+	protected int vStatementsNumber;
 
 	public Expression getCondition() {
 		return this.condition;
@@ -30,5 +38,43 @@ public class PreBlockstarter extends PreStatement{
 	@Override
 	public void accept(ASTNodeVisitor visitor) {
 		visitor.visit(this);
+	}
+
+	public void addVariableStatement(ASTNode node) {
+		if (variableStatements == null)
+			variableStatements = new LinkedList<ASTNode>();
+		this.setVStatementsNumber(variableStatements.size());
+		variableStatements.add(node);
+	}
+
+	public int getVariableStatementsCount() {
+		if (variableStatements == null)
+			return 0;
+		return variableStatements.size();
+	}
+
+	public ASTNode getVariableStatement(int i) {
+		if (variableStatements == null)
+			return null;
+
+		ASTNode retval;
+		try {
+			retval = variableStatements.get(i);
+		} catch (IndexOutOfBoundsException ex) {
+			return null;
+		}
+		return retval;
+	}
+
+	public ASTNode popLastVariableStatement() {
+		return variableStatements.removeLast();
+	}
+
+	public int getVStatementsNumber() {
+		return this.vStatementsNumber;
+	}
+
+	public void setVStatementsNumber(int num) {
+		this.vStatementsNumber = num;
 	}
 }
