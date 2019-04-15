4c986fb2a325631cd4d3c2e88730669e845d8a35
Lea <Lea@73V.speedport.ip>
2019-04-15 14:33:25 +0200

    Minor refinements

--- projects/extensions/joern-fuzzyc/src/test/java/tests/languages/c/parseTreeToAST/ModuleBuildersTest.java
+++ projects/extensions/joern-fuzzyc/src/test/java/tests/languages/c/parseTreeToAST/ModuleBuildersTest.java
@@ -14,6 +14,7 @@ import ast.declarations.ClassDefStatement;
 import ast.declarations.IdentifierDecl;
 import ast.functionDef.FunctionDefBase;
 import ast.functionDef.ParameterBase;
+import ast.preprocessor.PreBlockstarter;
 import ast.preprocessor.PreStatementBase;
 import ast.statements.IdentifierDeclStatement;
 import parsing.TokenSubStream;
@@ -229,13 +230,14 @@ public class ModuleBuildersTest {
 	}
 	
 	@Test
-	public void preprocessorModuleBuilderASTNestingWithNestedIFsTest() {
+	public void preprocessorModuleBuilderVariabilityNestingWithNestedIFsTest() {
 		String input = "#if (a) \n int a; \n #if (b) \n double b; \n #else \n long c; \n #endif \n #endif";
 		List<ASTNode> codeItems = parseInput(input);
 		//First PreIfStatement is the last node on the stack
-		PreStatementBase codeItem = (PreStatementBase) codeItems.get(4);	
+		PreBlockstarter codeItem = (PreBlockstarter) codeItems.get(4);	
 		assertEquals("PreIfStatement", codeItem.getTypeAsString());
-		//assertEquals("PreIfStatement", codeItem.getChild(2).getTypeAsString());
+		assertEquals("IdentifierDeclStatement", codeItem.getVariableStatement(0).getTypeAsString());
+		assertEquals("#if ( b )", codeItem.getVariableStatement(1).getEscapedCodeStr());
 		assertEquals("PreEndIfStatement", codeItem.getChild(2).getTypeAsString());
 	}
 	
@@ -245,16 +247,16 @@ public class ModuleBuildersTest {
 		String input = "#if (modulefoo < 5) \n int foo(){ int modulei1;} \n #elif (modulefoo > 5) \n double foo(){ double modulei1;} \n #else \n long foo(){ long modulei1;} \n #endif";
 		List<ASTNode> codeItems = parseInput(input);
 		//First PreIfStatement is the last node on the stack
-		PreStatementBase codeItem = (PreStatementBase) codeItems.get(3);	
+		PreBlockstarter codeItem = (PreBlockstarter) codeItems.get(3);	
 		assertEquals("PreIfStatement", codeItem.getTypeAsString());
-//		assertEquals("FunctionDef", codeItem.getChild(2).getTypeAsString());
-		codeItem = (PreStatementBase) codeItem.getChild(2);	
+		assertEquals("FunctionDef", codeItem.getVariableStatement(0).getTypeAsString());
+		codeItem = (PreBlockstarter) codeItem.getChild(2);	
 		assertEquals("PreElIfStatement", codeItem.getTypeAsString());
-//		assertEquals("FunctionDef", codeItem.getChild(2).getTypeAsString());
-		codeItem = (PreStatementBase) codeItem.getChild(2);			
+		assertEquals("foo ()", codeItem.getVariableStatement(0).getEscapedCodeStr());
+		codeItem = (PreBlockstarter) codeItem.getChild(2);			
 		assertEquals("PreElseStatement", codeItem.getTypeAsString());
-//		assertEquals("FunctionDef", codeItem.getChild(1).getTypeAsString());
-		codeItem = (PreStatementBase) codeItem.getChild(1);		
+		assertEquals("FunctionDef", codeItem.getVariableStatement(0).getTypeAsString());
+		codeItem = (PreBlockstarter) codeItem.getChild(1);		
 		assertEquals("PreEndIfStatement", codeItem.getTypeAsString());
 	}
 
