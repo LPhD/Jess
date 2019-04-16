5defbd7ef728aeb3a57c4a1576218ddc96a62c4b
gerling <gerling@Gerling-7490.sse.local>
2019-04-16 18:18:11 +0200

    Improved plugin error handling

--- projects/octopus/src/main/java/octopus/api/plugin/PluginExecutor.java
+++ projects/octopus/src/main/java/octopus/api/plugin/PluginExecutor.java
@@ -10,38 +10,35 @@ import octopus.server.pluginInterface.PluginLoader;
 
 public class PluginExecutor {
 
-	public Object executePlugin(String pluginName, String pluginClass)
-	{
+	public Object executePlugin(String pluginName, String pluginClass) {
 		return executePlugin(pluginName, pluginClass, null);
 	}
 
-	public Object executePlugin(String pluginName, String pluginClass, JSONObject settings)
-	{
+	public Object executePlugin(String pluginName, String pluginClass, JSONObject settings) {
 		Plugin plugin;
 
-		if(!pluginName.equals("")){
+		if (!pluginName.equals("")) {
 			String pluginDir = OctopusEnvironment.PLUGINS_DIR.toString();
 			Path pathToJar = Paths.get(pluginDir, pluginName).toAbsolutePath();
 			plugin = PluginLoader.load(pathToJar, pluginClass);
-		}else{
+		} else {
 			plugin = PluginLoader.load(pluginClass);
 		}
 
 		if (plugin == null)
-			throw new RuntimeException(
-					"Error while loading plugin " + pluginName);
+			throw new RuntimeException("Error while loading plugin " + pluginName);
 
-		try
-		{
-			if(settings != null)
+		try {
+			if (settings != null)
 				plugin.configure(settings);
 			plugin.beforeExecution();
 			plugin.execute();
 			plugin.afterExecution();
 			return plugin.result();
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
+			System.out.println("Exception during plugin execution of: "+pluginName);
 			System.out.println(e.getMessage());
+			e.printStackTrace();
 			throw new RuntimeException(e.getMessage());
 		}
 	}
