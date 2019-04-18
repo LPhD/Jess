9cc5bfed6022da4b38981da73a959ae821d26443
gerling <gerling@Gerling-7490.sse.local>
2019-04-18 14:55:04 +0200

    Added try/catch in decompress mechanism

--- projects/octopus/src/main/java/octopus/server/decompressor/TarballDecompressor.java
+++ projects/octopus/src/main/java/octopus/server/decompressor/TarballDecompressor.java
@@ -14,33 +14,35 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
 public class TarballDecompressor {
 
-	public void decompress(String tarballFilename, String outputDirectory) throws FileNotFoundException, IOException
-	{
-		if(!outputDirectory.endsWith(File.separator))
+	public void decompress(String tarballFilename, String outputDirectory) throws FileNotFoundException, IOException {
+		if (!outputDirectory.endsWith(File.separator))
 			outputDirectory = outputDirectory + File.separator;
 
 		TarArchiveInputStream tarIn = createTarInputStreamForFile(tarballFilename);
 
 		TarArchiveEntry entry;
-		while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null)
-		{
+		while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
 			String outputFilename = outputDirectory + entry.getName();
 
-			if(outputFilename.contains("pax_global_header"))
+			if (outputFilename.contains("pax_global_header"))
 				continue;
 
 			if (entry.isDirectory()) {
 				createOutputSubDirectory(outputFilename);
 				continue;
 			}
-			decompressFileToOutputDirectory(tarIn, outputFilename);
+			try {
+				decompressFileToOutputDirectory(tarIn, outputFilename);
+			} catch (Exception e) {
+				System.err.println("Error while decompressing file");
+				e.printStackTrace();
+			}
+
 		}
 		tarIn.close();
 	}
 
-	private TarArchiveInputStream createTarInputStreamForFile(String tarballFilename)
-			throws FileNotFoundException, IOException
-	{
+	private TarArchiveInputStream createTarInputStreamForFile(String tarballFilename) throws FileNotFoundException, IOException {
 		FileInputStream fin = new FileInputStream(tarballFilename);
 		BufferedInputStream in = new BufferedInputStream(fin);
 		GzipCompressorInputStream gzIn = new GzipCompressorInputStream(in);
@@ -48,22 +50,27 @@ public class TarballDecompressor {
 		return tarIn;
 	}
 
-	private void createOutputSubDirectory(String outputFilename)
-	{
+	private void createOutputSubDirectory(String outputFilename) {
 		File f = new File(outputFilename);
-	    f.mkdirs();
+		f.mkdirs();
 	}
 
-	private void decompressFileToOutputDirectory(TarArchiveInputStream tarIn, String outputFilename) throws IOException
-	{
-		final int BUFFER_SIZE = 2048;
+	private void decompressFileToOutputDirectory(TarArchiveInputStream tarIn, String outputFilename) throws IOException {
+		final int BUFFER_SIZE = 4096;
 		byte data[] = new byte[BUFFER_SIZE];
 		int bytesRead;
 
+		System.out.println("outputFilename: " + outputFilename);
+
 		FileOutputStream fos = new FileOutputStream(outputFilename);
+
+		System.out.println("Buffer");
+
 		BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER_SIZE);
-		while ((bytesRead = tarIn.read(data, 0, BUFFER_SIZE)) != -1)
-		{
+
+		System.out.println("ReadIn");
+
+		while ((bytesRead = tarIn.read(data, 0, BUFFER_SIZE)) != -1) {
 			dest.write(data, 0, bytesRead);
 		}
 		dest.close();
