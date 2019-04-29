3ada3f0a3859f9f4082bdc719db506b6f6ef8c5a
Lea <Lea@147.172.177.241>
2019-04-29 18:21:43 +0200

    Reworked adding of DB AST nodes inside functions, added variability edge drawing and include analysis

--- projects/extensions/joern-fuzzyc/src/main/java/outputModules/neo4j/exporters/Neo4JASTExporter.java
+++ projects/extensions/joern-fuzzyc/src/main/java/outputModules/neo4j/exporters/Neo4JASTExporter.java
@@ -13,70 +13,42 @@ import neo4j.batchInserter.GraphNodeStore;
 import neo4j.batchInserter.Neo4JBatchInserter;
 import outputModules.common.ASTExporter;
 
-public class Neo4JASTExporter extends ASTExporter
-{
+public class Neo4JASTExporter extends ASTExporter {
 	GraphNodeStore nodeStore;
 
-	public Neo4JASTExporter(GraphNodeStore aNodeStore)
-	{
+	public Neo4JASTExporter(GraphNodeStore aNodeStore) {
 		nodeStore = aNodeStore;
 	}
 
-	public void addASTToDatabase(ASTNode node)
-	{
-		addASTNode(node);
-		addASTChildren(node);
-	}
 
 	@Override
-	protected void addASTChildren(ASTNode node)
-	{
-		final int nChildren = node.getChildCount();
-
-		// HACK: not sure why, but adding AST-links backwards
-		// results in the edges being returned in the
-		// correct order by the database.
-
-		for (int i = nChildren - 1; i >= 0; i--)
-		{
-			ASTNode child = node.getChild(i);
-			addASTToDatabase(child);
-			addASTLink(node, child);
-		}
-	}
-
-	@Override
-	protected void addASTNode(ASTNode node)
-	{
-		ASTDatabaseNode astDatabaseNode = new ASTDatabaseNode();
-		astDatabaseNode.initialize(node);
-		astDatabaseNode.setCurrentFunction(currentFunction);
+	protected void addASTNode(ASTDatabaseNode astDatabaseNode) {
 		Map<String, Object> properties = astDatabaseNode.createProperties();
+		properties.put(NodeKeys.FUNCTION_ID, nodeStore.getIdForObject(currentFunction));
+		nodeStore.addNeo4jNode(astDatabaseNode, properties);
 
-		properties.put(NodeKeys.FUNCTION_ID,
-				nodeStore.getIdForObject(currentFunction));
-		nodeStore.addNeo4jNode(node, properties);
-
-		indexASTNode(node, properties);
-
-	}
-
-	private void indexASTNode(ASTNode node, Map<String, Object> properties)
-	{
-		nodeStore.indexNode(node, properties);
+		nodeStore.indexNode(astDatabaseNode, properties);
+		astDatabaseNode.setNodeId(nodeStore.getIdForObject(astDatabaseNode));
 	}
 
 	@Override
-	protected void addASTLink(ASTNode parent, ASTNode child)
-	{
-		RelationshipType rel = DynamicRelationshipType
-				.withName(EdgeTypes.IS_AST_PARENT);
+	protected void addASTLink(ASTNode parent, ASTNode child) {
+		RelationshipType rel = DynamicRelationshipType.withName(EdgeTypes.IS_AST_PARENT);
 
 		long parentId = nodeStore.getIdForObject(parent);
 		long childId = nodeStore.getIdForObject(child);
 		Map<String, Object> properties = null;
 
 		Neo4JBatchInserter.addRelationship(parentId, childId, rel, properties);
+	}
+	
+	/**
+	 * Link the given preStatementDatabaseNode (parentNodeID) with its block content (childNodeID)
+	 */
+	@Override
+	protected void drawVariabilityEdge(long parentNodeID, long childNodeID) {
+		RelationshipType rel = DynamicRelationshipType.withName(EdgeTypes.VARIABILITY);
+		Neo4JBatchInserter.addRelationship(parentNodeID, childNodeID, rel, null);
 	}
 
 }
--- projects/extensions/jpanlib/src/main/java/outputModules/common/ASTExporter.java
+++ projects/extensions/jpanlib/src/main/java/outputModules/common/ASTExporter.java
@@ -2,24 +2,69 @@ package outputModules.common;
 
 import ast.ASTNode;
 import ast.logical.statements.CompoundStatement;
+import ast.preprocessor.PreBlockstarter;
+import databaseNodes.ASTDatabaseNode;
 import databaseNodes.FunctionDatabaseNode;
+import includeAnalysis.IncludeAnalyzer;
 
 public abstract class ASTExporter {
 	protected FunctionDatabaseNode currentFunction;
 	protected int ancestorCompoundStatements;
 
 	public void addASTToDatabase(ASTNode node) {
-		enter(node);
-		addASTNode(node);
+		ASTDatabaseNode astDatabaseNode = new ASTDatabaseNode();
+		astDatabaseNode.initialize(node);
+		astDatabaseNode.setCurrentFunction(currentFunction);
+		astDatabaseNode.setInsideFunctionBlock(this.isInsideFunctionBlock());
+		
+		addASTNode(astDatabaseNode);
+		
+		// Link include statement with included file
+		if (node.getTypeAsString().equals("PreIncludeLocalFile")) {
+			IncludeAnalyzer.includeNodeList.add(astDatabaseNode);
+		}
+
+		// Look for statements that are inside an #ifdef block
+		if (node instanceof PreBlockstarter) {
+			// Set nodeID for Variability Analysis (this is only neccessary for #else and
+			// #elif nodes)
+			node.setNodeId(astDatabaseNode.getNodeId());
+			addVariableStatements(astDatabaseNode, (PreBlockstarter) node);
+		}
+		
 		visit(node);
 		addASTChildren(node);
 		leave(node);
 	}
 
-	public void setCurrentFunction(FunctionDatabaseNode func) {
-		currentFunction = func;
+	protected void visit(ASTNode node) {
+		if (node instanceof CompoundStatement) {
+			ancestorCompoundStatements++;
+		}
 	}
+	
+	/**
+	 * Look for nodes surrounded with the variability block of the current node and
+	 * add them to the db.
+	 * 
+	 * @param parent
+	 *            The current node.
+	 * @param astNodeParent
+	 *            The current node.
+	 */
+	private void addVariableStatements(ASTDatabaseNode preDBNode, PreBlockstarter preAstNode) {
+		final int nVariableStatements = preAstNode.getVariableStatementsCount();
+		for (int i = 0; i < nVariableStatements; i++) {
+			ASTNode vStatement = preAstNode.getVariableStatement(i);
+			long parentNodeID = preAstNode.getNodeId();
+			long childNodeID = vStatement.getNodeId();
 
+			drawVariabilityEdge(parentNodeID, childNodeID);
+			// System.out.println("Connected DB parent: "+preAstNode.getEscapedCodeStr()+"
+			// with DB child: "+vStatement.getEscapedCodeStr());
+		}
+	}
+	
 	protected void addASTChildren(ASTNode node) {
 
 		final int nChildren = node.getChildCount();
@@ -31,27 +76,23 @@ public abstract class ASTExporter {
 		}
 
 	}
-
-	protected abstract void addASTLink(ASTNode parent, ASTNode child);
-
-	protected abstract void addASTNode(ASTNode node);
-
-	protected void enter(ASTNode node) {
-	}
-
-	protected void visit(ASTNode node) {
-		if (node instanceof CompoundStatement) {
-			ancestorCompoundStatements++;
-		}
-	}
-
+	
 	protected void leave(ASTNode node) {
 		if (node instanceof CompoundStatement) {
 			ancestorCompoundStatements--;
 		}
 	}
+	
+	public void setCurrentFunction(FunctionDatabaseNode func) {
+		currentFunction = func;
+	}
 
 	public boolean isInsideFunctionBlock() {
 		return ancestorCompoundStatements > 0;
 	}
+	
+	protected abstract void addASTNode(ASTDatabaseNode astDatabaseNode);
+	protected abstract void addASTLink(ASTNode parent, ASTNode child);
+	protected abstract void drawVariabilityEdge(long parentNodeID, long childNodeID);
+
 }
