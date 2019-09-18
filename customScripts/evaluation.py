9f4bd56e69fa79119b065ef56ddcaf37127abd56
LPhD <gerling@sse.uni-hildesheim.de>
2019-09-18 14:23:40 +0200

    Refined input and output

--- customScripts/evaluation.py
+++ customScripts/evaluation.py
@@ -1,40 +1,22 @@
 #!/usr/bin/env python3
 from octopus.server.DBInterface import DBInterface
 import subprocess
+import os
 
-#Name of the project
-#projectName = 'Collection'
-projectName = 'EvoDiss.tar.gz'
-#projectName = 'ICSE'
-#projectName = 'JoernTest.tar.gz'
-#projectName = 'Linux3'
-#projectName = 'Marlin'
-#projectName = 'Revamp'
-#projectName = 'SPLC'
-#projectName = 'TestCommit'
+#Name and path of the project
+projectNameAndPath = {'Collection':'/Joern_Advanced/testProjects/Collection/src/', 
+'EvoDiss.tar.gz':'/home/lea/Downloads/EvoDiss/src/', 
+'ICSE':'/home/lea/Downloads/', 
+'JoernTest.tar.gz':'/home/lea/Downloads/Joern_Advanced/testProjects/Tests/', 
+'Linux3':'/home/lea/Downloads/linux-3.16.72/', 
+'Marlin':'/Marlin-1.1.9/', 
+'Revamp':'/home/lea/Downloads/Joern_Advanced/testProjects/', 
+'SPLC':'/Joern_Advanced/testProjects/SPLC/src/', 
+'TestCommit':'/src/'} 
 
-
-#Project specific paths
-#Collection path
-#projectPath = '/Joern_Advanced/testProjects/Collection/src/'
-#EvoDiss path
-projectPath = '/home/lea/Downloads/EvoDiss/src/'
-#ICSE path
-#projectPath = '/home/lea/Downloads/'
-#JoernTest path
-#projectPath = '/home/lea/Downloads/Joern_Advanced/testProjects/Tests/'
-#Linux3 path
-#projectPath = '/home/lea/Downloads/linux-3.16.72/'
-#Marlin path
-#projectPath = '/Marlin-1.1.9/'
-#Revamp path
-#projectPath = '/home/lea/Downloads/Joern_Advanced/testProjects/'
-#SPLC path
-#projectPath = '/Joern_Advanced/testProjects/SPLC/src/'
-#TestCommit path
-#projectPath = '/src/'
-
-#Assemble path
+#Assemble path 
+projectName = "EvoDiss.tar.gz"
+projectPath = projectNameAndPath[projectName]
 pathToPatch = 'Patch/'
 basePath = '/home/lea/Downloads/Joern_Advanced/projects/octopus/data/projects/'+projectName+'/src'
 pathToOriginalProject = basePath+projectPath
@@ -48,8 +30,10 @@ visibleStatementTypes = ['ClassDef', 'FunctionDef', 'CompoundStatement', 'DeclSt
 
 # Returns all AST vertices of the SemanticUnit that directly appear in the code (CFG nodes or direct children of file nodes)    
 def getVisibleNodes():
-    # Remove unneeded nodes
-    query = """g.V().has('type', within(%s)).id()""" % (visibleStatementTypes)  
+    # Remove unneeded nodes (we need to exclude IdentifierDeclStatement that have a ForInit as parent)
+    query = """g.V().has('type', within(%s))
+                .not(has('type', 'IdentifierDeclStatement').in(AST_EDGE).has('type', 'ForInit'))
+                .id()""" % (visibleStatementTypes) 
     result = db.runGremlinQuery(query)
     return result  
 
@@ -68,6 +52,11 @@ print("Compare with original source code...")
 with open('EvaluationResult.txt', 'w') as f:
     # No history, ignore whitespaces
     subprocess.call(["git", "diff", "--no-index", "-w", pathToOriginalProject, pathToPatch], stdout=f)
+
+if (os.stat("EvaluationResult.txt").st_size == 0):
+    print("* * * Evaluation was successful, no differences found * * *")
+else:
+    print("Found some differences, please look at EvaluationResult.txt for more details")
 
 
 
