ce4f4effd79cd98ad3460e8745712f1cc4f45795
Lea <Lea@10.8.0.26>
2019-10-16 18:45:34 +0200

    Return code of catch list

--- projects/extensions/jpanlib/src/main/java/ast/statements/blockstarters/CatchList.java
+++ projects/extensions/jpanlib/src/main/java/ast/statements/blockstarters/CatchList.java
@@ -5,27 +5,23 @@ import java.util.LinkedList;
 
 import ast.ASTNode;
 
-public class CatchList extends ASTNode implements Iterable<CatchStatement>
-{
-	
+public class CatchList extends ASTNode implements Iterable<CatchStatement> {
+
 	private LinkedList<CatchStatement> catchStatements = new LinkedList<CatchStatement>();
 
-	public String getEscapedCodeStr()
-	{
-		return "";
-	}
+//	public String getEscapedCodeStr() {
+//		return "";
+//	}
 
-	public int size()
-	{
+	public int size() {
 		return this.catchStatements.size();
 	}
-	
+
 	public CatchStatement getCatchStatement(int i) {
 		return this.catchStatements.get(i);
 	}
 
-	public void addCatchStatement(CatchStatement catchStatement)
-	{
+	public void addCatchStatement(CatchStatement catchStatement) {
 		this.catchStatements.add(catchStatement);
 		super.addChild(catchStatement);
 	}
