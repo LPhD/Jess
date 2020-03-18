40ac6fec2b390819de635956c0b1e85a2d558069
Lea <Lea@10.8.0.26>
2020-03-18 10:42:03 +0100

    More debug

--- projects/plugins/importer/src/main/java/joern/plugins/importer/CParserWrapper.java
+++ projects/plugins/importer/src/main/java/joern/plugins/importer/CParserWrapper.java
@@ -17,19 +17,28 @@ public class CParserWrapper {
 	}
 
 	public void initialize(String outputDir) {
+		System.out.println("Begin");
 		parser.setOutputDir(outputDir);
+		System.out.println("set output");
 		parser.setMultiFileOutput(multiFileOutput);
+		System.out.println("set multi output");
 		parser.initialize();
+		System.out.println("Initialize");
 		sourceFileWalker.addListener(parser);
+		System.out.println("add listener");
 	}
 
 	public void walkCodebase(String[] fileAndDirNames) {
+		System.out.println("Begin walking");
+		System.out.println(fileAndDirNames.toString());
 		try {
 			sourceFileWalker.walk(fileAndDirNames);
+			System.out.println("Walking end");
 		} catch (IOException err) {
 			System.err.println("Error walking source files: " + err.getMessage());
 		} finally {
 			parser.shutdown();
+			System.out.println("Shutdown");
 		}
 	}
 
