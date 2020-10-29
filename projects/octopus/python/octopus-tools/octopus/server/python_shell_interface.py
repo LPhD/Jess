331e16407c6049f38014d224c01be9ba2266555c
Lea <Lea@10.8.0.34>
2020-10-29 13:53:11 +0100

    ...

--- projects/octopus/python/octopus-tools/octopus/server/python_shell_interface.py
+++ projects/octopus/python/octopus-tools/octopus/server/python_shell_interface.py
@@ -100,7 +100,7 @@ class PythonShellInterface:
     def close(self):
         print("Try to close")
         OctopusShellConnection(self.host, self.port).close()
-        self.shell_manager.disconnect()
+        ShellManager(self.host, self.port).shell_manager.disconnect()
                    
 
     def runGremlinQuery(self, query):
